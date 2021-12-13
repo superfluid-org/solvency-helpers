ee59c059c4feefc574be5a4135dcce0506c60bba
didi <git@d10r.net>
2021-12-13 21:42:14 +0100

    added toga-checker

---
+++ toga-checker.js
@@ -0,0 +1,77 @@
+const togaABI = require("./abis/TOGA.json");
+const Web3 = require("web3");
+const axios = require("axios");
+const { wad4human } = require("@decentral.ee/web3-helpers");
+
+/*CONFIGS*/
+const NETWORKS = {
+    xdai: {
+        theGraphQueryUrl: "https://api.thegraph.com/subgraphs/name/superfluid-finance/protocol-v1-xdai",
+        web3ProviderUrl: process.env.XDAI_PROVIDER_URL || "http://xdai-mainnet-1.web3-nodes.superfluid.dev:8545",
+        toga: "0x0942570634A80bcd096873afC9b112A900492fd7"
+    },
+    matic: {
+        theGraphQueryUrl: "https://api.thegraph.com/subgraphs/name/superfluid-finance/protocol-v1-matic",
+        web3ProviderUrl: process.env.MATIC_PROVIDER_URL || "http://matic.hetzner-buck-1.web3-infra.superfluid.dev/rpc",
+        toga: "0x1b18770E5198983AF04B3735d01E09A57d27bD43"
+    },
+};
+
+
+
+async function getSuperTokens(graphAPI) {
+    const query = `query MyQuery {
+  tokens(where: {isSuperToken: true}) {
+    name
+    symbol
+    isSuperToken
+    isListed
+    id
+  }
+}`;
+    const res = await axios.post(graphAPI, { query });
+
+    if (res.status !== 200 || res.data.errors) {
+        console.error(res.data);
+        process.exit(1);
+    }
+
+    return res.data.data.tokens;
+}
+
+(async () => {
+    const networkName = process.env.NETWORK_NAME;
+    //for(networkName in NETWORKS) {
+        const web3 = new Web3(NETWORKS[networkName].web3ProviderUrl);
+        const toga = new web3.eth.Contract(togaABI, NETWORKS[networkName].toga);
+        const tblPIC = []; 
+        const tblNoPIC = [];
+        const superTokens = await getSuperTokens(NETWORKS[networkName].theGraphQueryUrl);
+
+        for (let i = 0; i < superTokens.length; i++) {
+            try {
+                const picInfo = await toga.methods.getCurrentPICInfo(superTokens[i].id).call();
+                if(picInfo.bond !== '0') {
+                    tblPIC.push({
+                        name: superTokens[i].name,
+                        symbol: superTokens[i].symbol,
+                        PIC: picInfo.pic,
+                        Bond: wad4human(picInfo.bond),
+                        ExitRatePerDay: wad4human(picInfo.exitRate * 3600 * 24) 
+                    })
+                } else {
+                    tblNoPIC.push({
+                        name: superTokens[i].name,
+                        symbol: superTokens[i].symbol,
+                    })
+                }
+            } catch(err) {
+                console.error(err);
+            }
+        }
+        console.log(`Network: ${networkName}`);
+        console.table(tblPIC, ["name", "symbol", "PIC", "Bond", "ExitRatePerDay"]);
+        //console.table(tblNoPIC);
+    //} 
+})();
+
