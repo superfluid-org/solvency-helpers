41df227175282aab77f213f2ec56d9b083cbd658
didi <git@d10r.net>
2022-05-18 00:22:11 +0200

    toga checker: use external network list (added Avalanche)

--- toga-checker.js
+++ toga-checker.js
@@ -2,31 +2,7 @@ const togaABI = require("./abis/TOGA.json");
 const Web3 = require("web3");
 const axios = require("axios");
 const { wad4human, toBN } = require("@decentral.ee/web3-helpers");
-
-/*CONFIGS*/
-const NETWORKS = {
-    xdai: {
-        theGraphQueryUrl: "https://api.thegraph.com/subgraphs/name/superfluid-finance/protocol-v1-xdai",
-        web3ProviderUrl: process.env.XDAI_PROVIDER_URL || "http://xdai-mainnet.web3-infra.superfluid.dev",
-        toga: "0xb7DE52F4281a7a276E18C40F94cd93159C4A2d22"
-    },
-    matic: {
-        theGraphQueryUrl: "https://api.thegraph.com/subgraphs/name/superfluid-finance/protocol-v1-matic",
-        web3ProviderUrl: process.env.MATIC_PROVIDER_URL || "http://polygon-mainnet.web3-infra.superfluid.dev",
-        toga: "0x6AEAeE5Fd4D05A741723D752D30EE4D72690A8f7"
-    },
-    opmainnet: {
-        theGraphQueryUrl: "https://api.thegraph.com/subgraphs/name/superfluid-finance/protocol-v1-optimism-mainnet",
-        web3ProviderUrl: process.env.MATIC_PROVIDER_URL || "http://optimism-mainnet.web3-infra.superfluid.dev",
-        toga: "0xA3c8502187fD7a7118eAD59dc811281448946C8f"
-    },
-    arbone: {
-        theGraphQueryUrl: "https://api.thegraph.com/subgraphs/name/superfluid-finance/protocol-v1-arbitrum-one",
-        web3ProviderUrl: process.env.MATIC_PROVIDER_URL || "http://arbitrum-one.web3-infra.superfluid.dev",
-        toga: "0xFC63B7C762B10670Eda15cF3ca3970bCDB28C9eF"
-    },
-};
-
+const sfMeta = require("superfluid-metadata");
 
 
 async function getSuperTokens(graphAPI) {
@@ -51,39 +27,39 @@ async function getSuperTokens(graphAPI) {
 
 (async () => {
     const networkName = process.env.NETWORK_NAME;
-    //for(networkName in NETWORKS) {
-        const web3 = new Web3(NETWORKS[networkName].web3ProviderUrl);
-        const toga = new web3.eth.Contract(togaABI, NETWORKS[networkName].toga);
-        const tblPIC = []; 
-        const tblNoPIC = [];
-        const superTokens = await getSuperTokens(NETWORKS[networkName].theGraphQueryUrl);
+    const network = sfMeta.getNetworkByName(networkName);
+    const rpcUrl = `http://${network.name}.web3-infra.superfluid.dev`;
 
-        for (let i = 0; i < superTokens.length; i++) {
-            try {
-                const picInfo = await toga.methods.getCurrentPICInfo(superTokens[i].id).call();
-                if(picInfo.bond !== '0' || picInfo.pic !== "0x0000000000000000000000000000000000000000") {
-                    tblPIC.push({
-                        name: superTokens[i].name,
-                        symbol: superTokens[i].symbol,
-                        PIC: picInfo.pic,
-                        Bond: wad4human(picInfo.bond),
-                        ExitRatePerDay: wad4human(toBN(picInfo.exitRate).mul(toBN(3600 * 24))) 
-                    });
-                } else {
-                    tblNoPIC.push({
-                        name: superTokens[i].name,
-                        symbol: superTokens[i].symbol,
-                    })
-                }
-            } catch(err) {
-                console.error(err);
+    const web3 = new Web3(rpcUrl);
+    const toga = new web3.eth.Contract(togaABI, network.contractsV1.toga);
+    const tblPIC = [];
+    const tblNoPIC = [];
+    const superTokens = await getSuperTokens(network.subgraphV1.hostedEndpoint);
+
+    for (let i = 0; i < superTokens.length; i++) {
+        try {
+            const picInfo = await toga.methods.getCurrentPICInfo(superTokens[i].id).call();
+            if(picInfo.bond !== '0' || picInfo.pic !== "0x0000000000000000000000000000000000000000") {
+                tblPIC.push({
+                    name: superTokens[i].name,
+                    symbol: superTokens[i].symbol,
+                    PIC: picInfo.pic,
+                    Bond: wad4human(picInfo.bond),
+                    ExitRatePerDay: wad4human(toBN(picInfo.exitRate).mul(toBN(3600 * 24)))
+                });
+            } else {
+                tblNoPIC.push({
+                    name: superTokens[i].name,
+                    symbol: superTokens[i].symbol,
+                })
             }
+        } catch(err) {
+            console.error(err);
         }
-        console.log(`Network: ${networkName} - TOGAv2`);
-        console.log('```');
-        console.table(tblPIC, ["name", "symbol", "PIC", "Bond", "ExitRatePerDay"]);
-        console.log('```');
-        //console.table(tblNoPIC);
-    //} 
+    }
+    console.log(`Network: ${networkName} - TOGAv2`);
+    console.log('```');
+    console.table(tblPIC, ["name", "symbol", "PIC", "Bond", "ExitRatePerDay"]);
+    console.log('```');
 })();
 
