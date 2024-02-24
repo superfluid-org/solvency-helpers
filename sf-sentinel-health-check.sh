f7b7149e476bf27e6d824d7677d8fc3b34d3ba00
didi <git@d10r.net>
2024-02-24 18:55:29 +0100

    sf-sentinel-health-check.sh: use metadata, allow to run for testnets or mainnets

--- sf-sentinel-health-check.sh
+++ sf-sentinel-health-check.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-# first. update the manifest file:
-curl -sf https://raw.githubusercontent.com/superfluid-finance/superfluid-sentinel/master/manifest.json > manifest.json
+# usage: <cmd> <testnets|mainnets>
+# returns messages suited to be sent to slack if the sentinel for a network is down.
 
-# Run the jq command to extract the chainIds
-ids=$(jq '.networks | keys[]' manifest.json)
+kind=$1
+
+# using metadata
+curl -sf https://raw.githubusercontent.com/superfluid-finance/protocol-monorepo/dev/packages/metadata/networks.json > sf_networks.json
+
+if [[ "$kind" == "mainnets" ]]; then
+  ids=$(jq '.[] | select(.isTestnet == false and .isDeprecated == null) | .chainId' sf_networks.json)
+  domain="http://mainnet-sentinels.x.superfluid.dev"
+elif [[ "$kind" == "testnets" ]]; then
+  ids=$(jq '.[] | select(.isTestnet == true and .isDeprecated == null) | .chainId' sf_networks.json)
+  domain="http://testnet-sentinels.x.superfluid.dev"
+else
+  echo "usage: <cmd> <testnets|mainnets>"
+  exit 1
+fi
 
 for id in $ids; do
   # Remove the quotes from the ID string and convert to integer
@@ -12,9 +25,9 @@ for id in $ids; do
   # Calculate the port
   port=$(( idInt % 50000 + 10000))
 
-  metricsUrl="http://solvency-dev.x.superfluid.dev:$port"
-#  echo "checking chainId $idInt, metrics url $port..."
-  
+  metricsUrl="$domain:$port"
+  #echo "checking chainId $idInt, metrics url $port..."
+
   healthy=$(curl -fs $metricsUrl | jq ".healthy")
   if [[ $healthy != true ]]; then
     echo "<!channel> SF sentinel for chainId $idInt not healthy - see $metricsUrl (may be down if the process is dead)"
